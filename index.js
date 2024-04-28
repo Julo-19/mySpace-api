c4a13a129b28e0bcd944020a291c03a703f1cfc6
Julo-19 <souleymanekeita.barro@unchk.edu.sn>
2024-04-28 13:21:32 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const path = require('path')
+const express = require('express')
+var bodyParser = require('body-parser')
+const app = express()
+
+
+app.use(express.static(path.join(__dirname,'static')));
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json)
+
+app.get('/static/:filename', function (req, res) {
+
+    console.log("valeur parems", req.params)
+    res.sendFile(path.join(__dirname, 'static', req.params.filename));
+  })
+  
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(3000)
